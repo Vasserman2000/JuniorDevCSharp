8343f327257be9d68895b12f948cb52fa81dc9d1
Vasserman2000 <vasserman2000@gmail.com>
2019-03-23 22:38:44 +0200

    init

---
+++ JuniorDevCSharp/Enums.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuniorDevCSharp
+{
+    public class Enums
+    {
+        public enum Degree
+        {
+            BS,
+            MS,
+            BA,
+            MBA,
+            MA,
+            MFA,
+            PHD,
+            MD
+        }
+    }
+}
---
+++ JuniorDevCSharp/Models/Developer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static JuniorDevCSharp.Enums;
+
+namespace JuniorDeveloperExercisesCSharp.Models
+{
+    public class Developer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public Degree Degree { get; set; }
+        public int Experience { get; set; }
+        public bool IsWorking { get; set; }
+        public string CompanyName { get; set; }
+        public string JobTitle { get; set; }
+    }
+}
