6a6b6e5316eb3cc51459530f6f56d57e87433eb3
Vasserman2000 <vasserman2000@gmail.com>
2019-04-15 23:51:45 +0300

    TypeOf and Aggregate features of LINQ

--- JuniorDevCSharp/Program.cs
+++ JuniorDevCSharp/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using static JuniorDevCSharp.LINQ;
 using static JuniorDevCSharp.Enums.Position;
+using System.Collections;
 
 namespace JuniorDevCSharp
 {
@@ -14,6 +15,22 @@ namespace JuniorDevCSharp
         {
             // GET ALL JUNIOR DEVELOPERS
             var juniors = DevelopersList.Where(dev => dev.Job == JUNIOR);
+
+
+            IList mixList = new ArrayList();
+            mixList.Add(4);
+            mixList.Add("Shalom");
+            mixList.Add(91);
+            mixList.Add(true);
+            mixList.Add(11);
+            mixList.Add("Jhonny");
+            mixList.Add(19.184);
+            mixList.Add(new { Name = "Adam", Age = 30 });
+
+            int sum = mixList.OfType<int>().Sum();
+            string sentence = mixList.OfType<string>().Aggregate("", (current, next) => current + " " + next);
+            Console.WriteLine(sum);
+            Console.WriteLine(sentence);
         }
     }
 }
